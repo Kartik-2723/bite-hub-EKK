5813a301c5232b3d1495b386277ba431c7074df6
Kartik Mishra <kartikmishra2723@gmail.com>
2026-01-20 22:12:19 +0530

    Initial commit

---
+++ src/main/java/Kartik/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,12 @@
+package Kartik.restaurant.repositories;
+
+import Kartik.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant,String> {
+
+    //TODO: Custom queries
+
+}
